cc719d8deaa7c61650226c04286768ed2cf97a65
doanbv1 <133104362+doanbv1@users.noreply.github.com>
2023-05-17 16:43:18 +0700

    Initial commit

---
+++ app/src/main/java/com/doanbvph31058/btvnb4/ConstraintLayout.java
@@ -0,0 +1,14 @@
+package com.doanbvph31058.btvnb4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConstraintLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout);
+    }
+}
